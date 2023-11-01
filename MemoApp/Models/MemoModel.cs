d55b6973413f4982fe68e204c95f97485da09929
chany <chanyoo93@gmail.com>
2023-11-01 22:56:18 +0900

    apply mvvm pattern

---
+++ MemoApp/Models/MemoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoApp.Models
+{
+    public class MemoModel
+    {
+        public string MemoTitle { get; set; } = string.Empty;
+        public string Memo { get; set; } = string.Empty;
+        public DateTime SaveTime { get; set; }
+    }
+}
---
+++ MemoApp/Views/MemoView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MemoApp.Views
+{
+    /// <summary>
+    /// MemoView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MemoView : UserControl
+    {
+        public MemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
